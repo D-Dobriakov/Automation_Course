7c9639114d604ef22c9aba8e1bafeaee6e7f54f6
Denis Dobriakov <dobryakov.denis@gmail.com>
2016-07-05 16:45:28 +0300

    Update #2

--- module.rb
+++ module.rb
@@ -26,34 +26,33 @@ module OurModule
     @browser.find_element(:id, 'project_identifier').click
     @browser.find_element(:id, 'project_identifier').send_keys rand(9999)
     @browser.find_element(:name, 'commit').click
-
-
-    def create_issue (issue)
-
-      @browser.find_element(:class, 'new-issue').click
-
-      issue_type_dropdown = @browser.find_element(:id, 'issue_tracker_id')
-      option = Selenium::WebDriver::Support::Select.new(issue_type_dropdown)
-      option.select_by(:text, issue.to_s.capitalize)
-      @browser.find_element(:id, 'issue_subject').send_keys 'qwerty'
-      @browser.find_element(:name, 'commit').click
-    end
   end
-end
 
+  def create_issue (issue)
 
-def change_password
-  @browser.find_element(:css, '.icon.icon-passwd').click
-  @browser.find_element(:id, 'password').send_keys @password
-  new_pass='qwerty'
-  @browser.find_element(:id, 'new_password').send_keys new_pass
-  @browser.find_element(:id, 'new_password_confirmation').send_keys new_pass
-  @browser.find_element(:name, 'commit').click
-end
+    @browser.find_element(:class, 'new-issue').click
 
-def create_project_version
-  @browser.find_element(:id, 'tab-versions').click
-  @browser.find_element(:css, '#tab-content-versions .icon.icon-add').click
-  @browser.find_element(:id, 'version_name').send_keys 'mp'
-  @browser.find_element(:name, 'commit').click
+    issue_type_dropdown = @browser.find_element(:id, 'issue_tracker_id')
+    option = Selenium::WebDriver::Support::Select.new(issue_type_dropdown)
+    option.select_by(:text, issue.to_s.capitalize)
+    @browser.find_element(:id, 'issue_subject').send_keys 'qwerty'
+    @browser.find_element(:name, 'commit').click
+  end
+
+  def change_password
+    @browser.find_element(:css, '.icon.icon-passwd').click
+    @browser.find_element(:id, 'password').send_keys @password
+    new_pass='qwerty'
+    @browser.find_element(:id, 'new_password').send_keys new_pass
+    @browser.find_element(:id, 'new_password_confirmation').send_keys new_pass
+    @browser.find_element(:name, 'commit').click
+  end
+
+  def create_project_version
+    @browser.find_element(:id, 'tab-versions').click
+    @browser.find_element(:css, '#tab-content-versions .icon.icon-add').click
+    @browser.find_element(:id, 'version_name').send_keys 'mp'
+    @browser.find_element(:name, 'commit').click
+  end
+
 end
